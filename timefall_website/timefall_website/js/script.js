52e93c67e2d44f8894ade10f40cd5c660d11ba26
chronosacaria <chronos.droid@gmail.com>
2023-01-09 13:40:50 -0500

    Initial commit

---
+++ timefall_website/timefall_website/js/script.js
@@ -0,0 +1,21 @@
+window.onscroll = () =>{
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        dropdown.classList.remove('active')
+    })
+}
+
+document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+
+    let currentDropdown
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]')
+        currentDropdown.classList.toggle('active')
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove('active')
+    })
+})
